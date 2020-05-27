364e04966e18bc632b97d74a73502b6a443f49cb
Andrea Bentley <andrea@bentleytech.com>
2020-05-26 22:08:16 -0400

    Added connection to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
 const mysql = require("mysql");
+let connection;
 
 // create the connection information for the sql database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  // user: process.env.USER,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // user: process.env.USER,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
